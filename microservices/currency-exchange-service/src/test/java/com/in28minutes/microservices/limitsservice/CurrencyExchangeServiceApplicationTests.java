fef07cd697c315be301076aedf12d365c62235a8
Kmoha365 <151753363+Kmoha365@users.noreply.github.com>
2026-02-18 19:49:05 +0530

    Update CurrencyExchangeServiceApplicationTests.java

--- microservices/currency-exchange-service/src/test/java/com/in28minutes/microservices/limitsservice/CurrencyExchangeServiceApplicationTests.java
+++ microservices/currency-exchange-service/src/test/java/com/in28minutes/microservices/limitsservice/CurrencyExchangeServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.in28minutes.microservices.limitsservice;
+package com.in28minutes.microservices.CurrencyExchange;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class CurrencyExchangeServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
